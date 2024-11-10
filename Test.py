969d6ddd56e04bda85d17cd12cf425fd383d4ad8
sangu272 <sangeeta271713@gmail.com>
2024-11-11 00:03:57 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -16,7 +16,7 @@ from asyncio import sleep
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from logging import getLogger
-from VIPMUSIC.utils.VIP_ban import admin_filter
+from VIPMUSIC.utils.vip_ban import admin_filter
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
@@ -78,13 +78,13 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("SHUKLAMUSIC/assets/wel2.png")
+    background = Image.open("assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
     pfp = pfp.resize((575, 575))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=70)
-    welcome_font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=61)
+    font = ImageFont.truetype('assets/font.ttf', size=70)
+    welcome_font = ImageFont.truetype('assets/font.ttf', size=61)
     #draw.text((630, 540), f'ID: {id}', fill=(255, 255, 255), font=font)
     #
  #   draw.text((630, 300), f'NAME: {user}', fill=(255, 255, 255), font=font)
